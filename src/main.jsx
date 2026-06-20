e8801208a6e4e41ad1fd143672711ccb871a21b6
Jefferson Teles <jeffersontelesdeoliveira@gmail.com>
2026-06-19 22:49:59 -0300

    Remover ErrorBoundary temporariamente para identificar o erro real

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./i18n";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
 import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-      <Analytics />
-    </ErrorBoundary>
+    <App />
+    <Analytics />
   </React.StrictMode>,
 );
